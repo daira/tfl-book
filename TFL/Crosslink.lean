1166cc56f7d15bd87f3f06b523fddb876f63ba11
Daira-Emma Hopwood <daira@jacaranda.org>
2025-07-14 10:38:26 +0100

    Crosslink.lean: take the params explicitly.
    Signed-off-by: Daira-Emma Hopwood <daira@jacaranda.org>

--- TFL/Crosslink.lean
+++ TFL/Crosslink.lean
@@ -30,21 +30,18 @@ structure Crosslink where
   σ : ℕ+
   L : ℕ+
 
-/- The global parameters. -/
-variable (Params : Crosslink)
-
 /-- The Last Final bft-block for a given bc-block. -/
-def LF (c : BcChain) : BftChain :=
+def LF (params : Crosslink) (c : BcChain) : BftChain :=
   sorry
 
 /-- The snapshotted bc-block for a given bft-block. -/
-def snapshot (b : BftChain) : BcChain :=
+def snapshot (params : Crosslink) (b : BftChain) : BcChain :=
   match b.head? with
-  | some B => B.headers_bc ⎾_bc Params.σ
+  | some B => B.headers_bc ⎾_bc params.σ
   | none => []
 
-def candidate (H : BcChain) : BcChain :=
-  last_common_ancestor (snapshot Params (LF H)) (H ⎾_bc Params.σ)
+def candidate (params : Crosslink) (H : BcChain) : BcChain :=
+  last_common_ancestor (snapshot params (LF params H)) (H ⎾_bc params.σ)
 
 def scan {A : Type} (update : ℕ → A → A) (initial : A) : ℕ → A
   | 0 => initial
@@ -57,25 +54,26 @@ def scan_iterative {A : Type} (update : ℕ → A → A) (initial : A) (t : ℕ)
   state
 
 /-- Given `fin i s`, transition to `fin i t` where `t = s+1`. -/
-def update_fin (ch_i : Time → BcChain) (t : ℕ) (fin_i_s : BcChain) : BcChain :=
-  let N := candidate Params (ch_i t)
+def update_fin (params : Crosslink)
+    (ch_i : Time → BcChain) (t : ℕ) (fin_i_s : BcChain) : BcChain :=
+  let N := candidate params (ch_i t)
   if fin_i_s ≼_bc N then N else fin_i_s
 
 /- Extend `update_fin` to a time series. -/
-def fin (ch_i : Time → BcChain) (t : Time) : BcChain :=
-  scan (update_fin NC Params ch_i) [] t
+def fin (params : Crosslink) (ch_i : Time → BcChain) (t : Time) : BcChain :=
+  scan (update_fin NC params ch_i) [] t
 
-lemma fin_step (ch_i : Time → BcChain) (s : Time) :
-  fin NC Params ch_i (s+1) = update_fin NC Params ch_i (s+1) (fin NC Params ch_i s) := by rfl
+lemma fin_step (params : Crosslink) (ch_i : Time → BcChain) (s : Time)
+    : fin NC params ch_i (s+1) = update_fin NC params ch_i (s+1) (fin NC params ch_i s) := by rfl
 
-lemma candidate_trim (ch_i : Time → BcChain) (u : Time)
-    : candidate Params (ch_i u) ≼_bc ch_i u ⎾_bc Params.σ := by
-  let bc_snapshot := snapshot Params (LF (ch_i u))
-  let bc_confirmed := ch_i u ⎾_bc Params.σ
+lemma candidate_trim (params : Crosslink) (ch_i : Time → BcChain) (u : Time)
+    : candidate params (ch_i u) ≼_bc ch_i u ⎾_bc params.σ := by
+  let bc_snapshot := snapshot params (LF params (ch_i u))
+  let bc_confirmed := ch_i u ⎾_bc params.σ
   exact (lca_prefix NC bc_snapshot bc_confirmed).right
 
-lemma local_fin_depth (ch : Node → Time → BcChain) (honest : Node → Time → Bool)
-    : ∀ (i : Node) (t : Time) (honest_i : honest i t), ∃ (r : Time), r ≤ t ∧ fin NC Params (ch i) t ≼_bc ch i r ⎾_bc Params.σ := by
+lemma local_fin_depth (params : Crosslink) (ch : Node → Time → BcChain) (honest : Node → Time → Bool)
+    : ∀ (i : Node) (t : Time) (honest_i : honest i t), ∃ (r : Time), r ≤ t ∧ fin NC params (ch i) t ≼_bc ch i r ⎾_bc params.σ := by
   intro i t honest_i
   use t-1
   simp_all
@@ -84,20 +82,22 @@ lemma local_fin_depth (ch : Node → Time → BcChain) (honest : Node → Time �
 /--
 Assured Finality is Agreement on `V` composed with `fin`.
 -/
-public def AssuredFinality (V : Node → Time → BcChain) :=
-  BcAgreement NC (fun i t => fin NC Params (V i) t)
+public def AssuredFinality (params : Crosslink) (V : Node → Time → BcChain) :=
+  BcAgreement NC (fun i t => fin NC params (V i) t)
 
 /--
 Safety theorem: Assured Finality from BC Prefix Agreement.
 -/
-theorem assured_finality_from_bc_prefix_agreement (ch : Node → Time → BcChain) (honest : Node → Time → Bool)
-    (prefix_agreement : BcPrefixAgreement NC Params.σ ch honest) : AssuredFinality NC Params ch honest := by
+theorem assured_finality_from_bc_prefix_agreement (params : Crosslink)
+    (ch : Node → Time → BcChain) (honest : Node → Time → Bool)
+    (prefix_agreement : BcPrefixAgreement NC params.σ ch honest) : AssuredFinality NC params ch honest := by
   sorry
 
 /--
 Safety theorem: Assured Finality from BFT Final Agreement.
 -/
-theorem assured_finality_from_bft_final_agreement (V : Node → Time → BftChain × BcChain) (honest : Node → Time → Bool)
+theorem assured_finality_from_bft_final_agreement (params : Crosslink)
+    (V : Node → Time → BftChain × BcChain) (honest : Node → Time → Bool)
     (final_agreement : BftFinalAgreement (fun i t => (V i t).1) honest)
-    : AssuredFinality NC Params (fun i t => (V i t).2) honest := by
+    : AssuredFinality NC params (fun i t => (V i t).2) honest := by
   sorry
